111a33495c092997d2f6db41c095cd3b39a6b725
11328476-DCIT205 <paakofi.one@gmail.com>
2024-05-31 17:21:12 +0000

    filter images

--- assignment3/App.js
+++ assignment3/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text} >Hello, Devs</Text>
       <Text style={styles.text1} >14 tasks today</Text>
-      <TextInput style={styles.text2} placeholder={require("./images/Group 5 (1).png")}/>
+      <TextInput style={styles.text2} placeholder={"Search"}/>
+      <Image source={require('./images/person.png')} style={styles.Profile}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -32,4 +33,22 @@ const styles = StyleSheet.create({
     left:-150,
     fontSize: 10,
   },
+  text2:{
+    position: 'absolute',
+    top: 130,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    backgroundColor: '#f5f5f5',
+    width: 280,
+    left: 20
+  },
+  Profile:{
+top: -350,
+right: -145,
+backgroundColor: 'white'
+  }
 });
